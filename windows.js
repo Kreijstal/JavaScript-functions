e34d082a19dfb23fd8245417c1d9a2f15f9ded69
RainbowDangerDash <RainbowDangerDash@users.noreply.github.com>
2014-06-23 14:45:11 -0500

    some jscript syntax
    to remind me of how it is

---
+++ windows.js
@@ -0,0 +1,1 @@
+WSH.CreateObject("ADODB.Command");WSH.Echo("anyway..... please enter a line");str = WScript.StdIn.ReadLin;WSH.Echo("is this what you entered?"+str);oShell = WSH.CreateObject("WScript.Shell");oSHApp = WSH.CreateObject("Shell.Application");oShell.run("explorer /select,"+oShell.RegRead("HKCU\\Control Panel\\Desktop\\Wallpaper"));
