b622e13e31614aa6a8cbb697d287817d794c2ce8
Rainb <elektrischrainbow@gmail.com>
2014-07-25 07:04:56 +0000

    Hmhm, fixing bugs

--- Media Cache.js
+++ Media Cache.js
@@ -29,14 +29,18 @@ WScript.Echo(WshShell.CurrentDirectory);
   	var FileCollection = folder.Files;
 var strFileNames=[],strFileName,r=/f_(?:\d|[A-F])+/i,allFiles=[],currentFile={};
 	for(var match,objEnum=new Enumerator(FileCollection);!objEnum.atEnd();objEnum.moveNext()) {
+	
 	  if(/\\f_\d+/.test(strFileName=FSO.GetAbsolutePathName(objEnum.item()))){
+	  //WScript.Echo("Ehhh? "+objEnum.item());
 	   strFileNames.push(strFileName);
 	   if(match=readBinaryFile(strFileName).join('').match(/webm|ogg|riff|png|gif/i)){
+	   //WScript.Echo("Hue");
 	   if(currentFile.files){allFiles.push(currentFile);}
 	   currentFile={files:[strFileName],ext:match[0]};
 	   }else{currentFile.files.push(strFileName);}
 	   };
 	}
+allFiles.push(currentFile);
 function StringToArray(str){
 for(var arr=[],i=0,l=str.length;i<l;i++){
 arr[i]=str[i];
@@ -45,19 +49,22 @@ return arr;
 }
 //WScript.Echo(readBinaryFile(strFileNames[0]).join('').match(/webm|ogg/));
 var sav;
+/*WScript.Echo(allFiles);
+WScript.Echo("lel");*/
 for(var i=0,commands=[];i<allFiles.length;i++){
 commands.push("copy /b \""+allFiles[i].files.join('" + "')+'" ' +
 allFiles[i].files[0].match(/[\da-f]+$/i)[0]+
 '_'+
 allFiles[i].files[allFiles[i].files.length-1].match(/[\da-f]+$/i)[0]+
 '.'+allFiles[i].ext);
+//WScript.Echo(commands[commands.length-1]);
 }
 function ASCIIToEncodedBatch(str){
 return str.replace(/%/g,'%%').replace(/\^/g,'^^').replace(/[&\|<>"'`\=\\\/,;\(!\)0-9]/g,function(a){return '^'+a;}).replace(/[\n\r]/g,function(a){return '^'+a+a})
 }
 
 for(var i=0;i<commands.length;i++){
-WScript.Echo("cmd /c "+ASCIIToEncodedBatch(commands[i]));
+//WScript.Echo("cmd /c "+ASCIIToEncodedBatch(commands[i]));
 WshShell.Run("cmd /c "+ASCIIToEncodedBatch(commands[i]));
 }
 
